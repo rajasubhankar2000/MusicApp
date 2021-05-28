21080a7e6a13d79a4172be89305d29f72fc5806b
Subhankar <rajasubhankar2000@gmail.com>
2021-05-28 20:57:47 +0530

    Subhankar

---
+++ app/src/main/java/com/example/rajamusic/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.rajamusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    private Button play;
+    private Button pause;
+    private SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        play =findViewById(R.id.play);
+        pause =findViewById(R.id.pause);
+        seekBar =findViewById(R.id.seekBar);
+        mediaPlayer =MediaPlayer.create(this , R.raw.music);
+        mediaPlayer.start();
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               mediaPlayer.start();
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+        seekBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seekBar.setMax(mediaPlayer.getDuration());
+                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if (fromUser) {
+                        mediaPlayer.seekTo(progress);
+                        }
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+            }
+        });
+
+
+    }
+}
